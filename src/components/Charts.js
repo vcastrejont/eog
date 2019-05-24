627164531abfb003be86cb25f8827846dc2fda2b
Victor Castrejon <vcastrejon@nearsoft.com>
2019-05-24 12:57:04 -0700

    charts

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -19,43 +19,53 @@ const cardStyles = theme => ({
 });
 const CardHeader = withStyles(cardStyles)(CardHeaderRaw);
 
-const Charts = props => {
-  return (
-    <React.Fragment>
-      <CardHeader title="Charts " />
-      <CardContent>
-        <Chart
-          width={"100%"}
-          height={"400px"}
-          chartType="LineChart"
-          loader={<div>Loading Chart</div>}
-          data={[
-            ["x", "Temperature"],
-            [0, 274.1218550555868],
-            [1, 274.7189715925255],
-            [2, 275.3169013456079],
-            [3, 275.91550978316207],
-            [4, 276.51466222081496],
-            [5, 277.11422385179566],
-            [6, 278.91401463811906],
-            [7, 279.5138635887191]
-          ]}
-          options={{
-            hAxis: {
-              title: "Time"
-            },
-            vAxis: {
-              title: "Farenheit"
-            },
-            title: "Sample Drone Temperature",
-            curveType: "function"
-          }}
-          rootProps={{ "data-testid": "1" }}
-        />
-      </CardContent>
-    </React.Fragment>
-  );
-};
+export class Charts extends React.Component {
+  componentDidMount() {
+    this.props.fetchDrone();
+    this.timer = setInterval(() => this.props.fetchDrone(), 5000);
+  }
+  render() {
+    const { data } = this.props;
+
+    if (!data) {
+      return <Loading />;
+    }
+
+    return (
+      <React.Fragment>
+        <CardHeader title="Charts " />
+        <CardContent>
+          <Chart
+            width={"100%"}
+            height={"400px"}
+            chartType="LineChart"
+            loader={<div>Loading Chart</div>}
+            data={[["x", "Farenheit"], ...data]}
+            options={{
+              hAxis: {
+                title: "Time"
+              },
+              vAxis: {
+                title: "Temperature "
+              },
+              title: "Sample Drone Temperature",
+              curveType: "function",
+              legend: { position: "right" },
+              axes: {
+                // Adds labels to each axis; they don't have to match the axis names.
+                y: {
+                  Temps: { label: "Temps (Celsius)" },
+                  Daylight: { label: "Daylight" }
+                }
+              }
+            }}
+            rootProps={{ "data-testid": "1" }}
+          />
+        </CardContent>
+      </React.Fragment>
+    );
+  }
+}
 
 const mapDispatchToProps = dispatch => {
   const actions = bindActionCreators(
@@ -69,11 +79,11 @@ const mapDispatchToProps = dispatch => {
 };
 
 const mapStateToProps = state => {
-  const last = state.drone.data.length;
   return {
     loading: state.drone.loading,
-    data: state.drone.data[last - 1],
-    last: state.drone.last
+    data: state.drone.data.map((item, index) => {
+      return [new Date(item.timestamp * 1000).toString(), item.metric];
+    })
   };
 };
 
